e875255a229120412de9ea2004cc8e29a3605058
Soprano7 <144703389+Soprano7@users.noreply.github.com>
2024-07-24 16:05:59 +0300

    s

--- src/utils/evaluateExpression.ts
+++ src/utils/evaluateExpression.ts
@@ -2,7 +2,7 @@ export const evaluateExpression = (expression: string): string => {
     try {
         let expr = expression.replace(/√/g, 'Math.sqrt');
         expr = expr.replace(/(\d+)%/g, '($1 * 0.01)');
-        let result = Function(`"use strict"; return (${expr})`)();
+        const result = Function(`"use strict"; return (${expr})`)();
         return String(result);
     } catch (error) {
         return 'Error';
